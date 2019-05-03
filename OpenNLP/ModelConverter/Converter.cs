a2eb8b73677bf210f8ac5797c07b3e6708c5fcb0
Rojan Gharibpour <sojaner@gmail.com>
2019-05-03 22:45:20 +0200

    Figured the training data for training the chunker.

--- OpenNLP/ModelConverter/Converter.cs
+++ OpenNLP/ModelConverter/Converter.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using OpenNLP.Tools.Chunker;
 using OpenNLP.Tools.PosTagger;
 using OpenNLP.Tools.SentenceDetect;
 using OpenNLP.Tools.Tokenize;
@@ -18,7 +22,7 @@ namespace ModelConverter
         /// </summary>
         static void Main(string[] args)
         {
-            string project = "I rollen som försäkringsförmedlare hjälper du kunder med att hitta den bästa lösningen utifrån deras behov. I samband med utbildningsstart får du genomgå certifiering för försäkringsförmedling där du får alla verktyg för att du ska känna dig trygg i din roll. Du agerar som specialist där du ger professionell rådgivning och tar fram olika försäkringslösningar till privatkunder. ";
+            string project = "Äktenskapet och familjen är en gammal institution, som funnits sedan 1800-talet. I rollen som försäkringsförmedlare hjälper du kunder med att hitta den bästa lösningen utifrån deras behov. I samband med utbildningsstart får du genomgå certifiering för försäkringsförmedling där du får alla verktyg för att du ska känna dig trygg i din roll. Du agerar som specialist där du ger professionell rådgivning och tar fram olika försäkringslösningar till privatkunder. ";
 
             JavaBinaryGisModelReader sentenceModelReader = new JavaBinaryGisModelReader(@".\bins\sent.bin");
 
@@ -40,11 +44,21 @@ namespace ModelConverter
 
             string[] sentences = detector.SentenceDetect(project);
 
+            PlainTextByLineDataReader dataReader = new PlainTextByLineDataReader(File.OpenText(@".\bins\chunker.conll"));
+
+            BasicEventReader reader = new BasicEventReader(dataReader);
+
+            GisModel gisModel = MaximumEntropyChunker.Train(reader);
+
+            MaximumEntropyChunker chunker = new MaximumEntropyChunker(gisModel);
+
             foreach (string sentence in sentences)
             {
                 string[] tokens = tokenizer.Tokenize(sentence);
 
                 string[] tags = tagger.Tag(tokens);
+
+                string[] strings = chunker.Chunk(tokens,tags);
             }
 
             if (args.Length != 1)
