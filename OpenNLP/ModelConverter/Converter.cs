5b01710906db50112b1d3a08d963b1b5b34bd635
Rojan Gharibpour <sojaner@gmail.com>
2019-05-05 00:48:52 +0200

    Trying to figure out the training.

--- OpenNLP/ModelConverter/Converter.cs
+++ OpenNLP/ModelConverter/Converter.cs
@@ -2,11 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using OpenNLP.Tools.Chunker;
 using OpenNLP.Tools.PosTagger;
-using OpenNLP.Tools.SentenceDetect;
-using OpenNLP.Tools.Tokenize;
 using SharpEntropy;
 using SharpEntropy.IO;
 
@@ -22,46 +20,12 @@ namespace ModelConverter
         /// </summary>
         static void Main(string[] args)
         {
-            string project = "Äktenskapet och familjen är en gammal institution, som funnits sedan 1800-talet. I rollen som försäkringsförmedlare hjälper du kunder med att hitta den bästa lösningen utifrån deras behov. I samband med utbildningsstart får du genomgå certifiering för försäkringsförmedling där du får alla verktyg för att du ska känna dig trygg i din roll. Du agerar som specialist där du ger professionell rådgivning och tar fram olika försäkringslösningar till privatkunder. ";
+            //https://cl.lingfil.uu.se/~nivre/swedish_treebank/
 
-            JavaBinaryGisModelReader sentenceModelReader = new JavaBinaryGisModelReader(@".\bins\sent.bin");
-
-            GisModel sentenceModel = new GisModel(sentenceModelReader);
-
-            MaximumEntropySentenceDetector detector = new MaximumEntropySentenceDetector(sentenceModel);
-
-            JavaBinaryGisModelReader tokenModelReader = new JavaBinaryGisModelReader(@".\bins\token.bin");
-
-            GisModel tokenModel = new GisModel(tokenModelReader);
-
-            MaximumEntropyTokenizer tokenizer = new MaximumEntropyTokenizer(tokenModel);
-
-            JavaBinaryGisModelReader posModelReader = new JavaBinaryGisModelReader(@".\bins\pos.bin");
-
-            GisModel posModel = new GisModel(posModelReader);
-
-            MaximumEntropyPosTagger tagger = new MaximumEntropyPosTagger(posModel);
-
-            string[] sentences = detector.SentenceDetect(project);
-
-            PlainTextByLineDataReader dataReader = new PlainTextByLineDataReader(File.OpenText(@".\bins\chunker.conll"));
-
-            BasicEventReader reader = new BasicEventReader(dataReader);
-
-            GisModel gisModel = MaximumEntropyChunker.Train(reader);
-
-            MaximumEntropyChunker chunker = new MaximumEntropyChunker(gisModel);
-
-            foreach (string sentence in sentences)
-            {
-                string[] tokens = tokenizer.Tokenize(sentence);
-
-                string[] tags = tagger.Tag(tokens);
-                
-                // https://cl.lingfil.uu.se/~nivre/research/Talbanken05.html
-                // Compare the list pos in the file with the ones for the tagger
-                string[] strings = chunker.Chunk(tokens,tags);
-            }
+            //TODO: Use the train data for training and use the test data to confirm the accuracy
+            //TODO: Use the proper EventReader/TextReader
+            //TODO: Figure out the problem with the +A/+B tokens
+            //TODO: Update the OpenNLP code with the latest available on https://github.com/AlexPoint/OpenNlp
 
             if (args.Length != 1)
             {
