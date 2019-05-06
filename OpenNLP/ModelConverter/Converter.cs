742f3214b31ed22b2af50df24f2008f340bbce8e
Rojan Gharibpour <sojaner@gmail.com>
2019-05-06 20:14:09 +0200

    Updated the trainer.

--- OpenNLP/ModelConverter/Converter.cs
+++ OpenNLP/ModelConverter/Converter.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using OpenNLP.Tools.Chunker;
 using OpenNLP.Tools.PosTagger;
 using SharpEntropy;
 using SharpEntropy.IO;
@@ -19,7 +22,7 @@ namespace ModelConverter
         {
             //https://cl.lingfil.uu.se/~nivre/swedish_treebank/
 
-            int iterations = 0;
+            /*int iterations = 0;
 
             float percentage = 0;
 
@@ -41,7 +44,16 @@ namespace ModelConverter
                 {
                     string[] parts = line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(part => part.Split("_")[0]).ToArray();
 
-                    string[] tags = tagger.Tag(parts);
+                    string[] tags = new string[parts.Length];
+
+                    try
+                    {
+                        tags = tagger.Tag(parts);
+                    }
+                    catch
+                    {
+                        //
+                    }
 
                     string sentence = "";
 
@@ -60,13 +72,83 @@ namespace ModelConverter
 
             PlainTextGisModelWriter writer = new PlainTextGisModelWriter();
 
-            writer.Persist(model, @".\pos.model");
+            writer.Persist(model, @".\pos.model");*/
+
+            /*int iterations = 0;
+
+            float percentage = 0;
+
+            GisModel model = null;
+
+            while (percentage < 90f)
+            {
+                iterations += 500;
+
+                model = MaximumEntropyChunker.Train(new ChunkerEventReader(new PlainTextByLineDataReader(File.OpenText(@".\data\chunker-talbanken-dep-train.conll"))), iterations, 5);
+
+                MaximumEntropyChunker chunker = new MaximumEntropyChunker(model);
+
+                string[] lines = File.ReadAllLines(@".\data\chunker-talbanken-dep-test.conll");
+
+                IEnumerator enumerator = lines.GetEnumerator();
+
+                List<string> words = new List<string>();
+
+                List<string> tags = new List<string>();
+
+                List<string> chunks = new List<string>();
+
+                int total = 0;
+
+                int equals = 0;
+
+                while (enumerator.MoveNext())
+                {
+                    if ((string)enumerator.Current != "")
+                    {
+                        string[] parts = ((string)enumerator.Current).Split(" ");
+
+                        words.Add(parts[0]);
+
+                        tags.Add(parts[1]);
+
+                        chunks.Add(parts[2]);
+                    }
+                    else
+                    {
+                        string[] newChunks = new string[0];
+
+                        try
+                        {
+                            newChunks = chunker.Chunk(words.ToArray(), tags.ToArray());
+                        }
+                        catch
+                        {
+                            //
+                        }
+
+                        equals += string.Join(" ", newChunks) == string.Join(" ", chunks) ? 1 : 0;
+
+                        total++;
+
+                        words.Clear();
+
+                        tags.Clear();
+
+                        chunks.Clear();
+                    }
+                }
+
+                percentage = (equals / (float)total) * 100f;
+            }
+
+            PlainTextGisModelWriter writer = new PlainTextGisModelWriter();
+
+            writer.Persist(model, @".\chu.model");*/
 
             //TODO: Use the train data for training and use the test data to confirm the accuracy
             //TODO: Use the proper EventReader/TextReader
             //TODO: Figure out the problem with the +A/+B tokens
-
-            return;
 
             if (args.Length != 1)
             {
